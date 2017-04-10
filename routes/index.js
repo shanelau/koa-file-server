12f1fa03d92b3f512e0b7be6ba68287c9d62941a
刘兴 <hzliuxing1@corp.netease.com>
2017-04-10 15:00:20 +0800

    Fix
    1. Format util.js
    2. Add http status for render error

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   req.file('avatar').upload(function (err, uploadedFiles) {
     if (err) return res.send(500, err);
     return res.json({
